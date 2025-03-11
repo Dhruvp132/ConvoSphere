329cbb00107b319ee8c0157d985ae3f4836dd0de
Dhruvp132 <106880373+Dhruvp132@users.noreply.github.com>
2025-03-11 15:55:42 +0530

    frontend

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -16,7 +16,8 @@ declare global {
   }
 }
 
-const URL = "http://localhost:3000"
+// const URL = "http://localhost:3000"
+const URL = "https://convosphere-mep0.onrender.com"
 
 export const Room = ({
   name,
